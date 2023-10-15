a737dc6954fad7c76734297f6476060ac839ad29
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-15 13:57:47 +0100

    feat: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import pipe from "public/link-05.svg";
 import share from "public/share-04.svg";
 import transform from "public/transform.svg";
 import user from "public/user-01.svg";
+import settings from "public/settings-01.svg";
 import book from "public/book.svg";
 interface Item {
   id: number;
@@ -41,6 +42,31 @@ const items: Item[] = [
     text: "Transformations",
   },
 ];
+
+const items2: Item[] = [
+  {
+    id: 1,
+    img: user,
+    text: "Account",
+  },
+  {
+    id: 2,
+    img: settings,
+    text: "Settings",
+  },
+];
+const items3: Item[] = [
+  {
+    id: 1,
+    img: book,
+    text: "Help",
+  },
+  {
+    id: 2,
+    img: book,
+    text: "Documentation",
+  },
+];
 const Navbar = () => {
   return (
     <nav className="w-[20%] bg-zinc-50 px-8 hidden md:flex flex-col gap-6 h-full py-12">
@@ -52,13 +78,43 @@ const Navbar = () => {
           <h3 className="font-semibold">Sigflow</h3>
           <Image src={selector} alt="" />
         </div>
-        <div className="flex flex-col gap-4 my-8">
+        <div className="flex flex-col gap-4 py-10  ">
           {items.map((item) => (
             <div
               key={item.id}
               className={`${
-                item.id === 5 ? "text-green bg-[#ECFDF3] p-2 rounded-md" : "text-[#828282]"
+                item.id === 5
+                  ? "text-green bg-[#ECFDF3] p-2 rounded-md"
+                  : "text-[#828282]"
               } font-semibold flex  gap-4 item-center`}
+            >
+              <Image src={item.img} alt={item.text} />
+              <p>{item.text}</p>
+            </div>
+          ))}
+        </div>
+        <hr className="text-[#E7E7E7] w-full p-0" />
+        <div className="flex flex-col gap-4 my-8">
+          {items2.map((item) => (
+            <div
+              key={item.id}
+              className={`${
+                item.id === 5
+                  ? "text-green bg-[#ECFDF3] p-2 rounded-md"
+                  : "text-[#828282]"
+              } font-semibold flex  gap-4 item-center`}
+            >
+              <Image src={item.img} alt={item.text} />
+              <p>{item.text}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-4 mb-8 mt-16">
+          {items3.map((item) => (
+            <div
+              key={item.id}
+              className={`
+                text-green  font-semibold flex  gap-4 item-center`}
             >
               <Image src={item.img} alt={item.text} />
               <p>{item.text}</p>
